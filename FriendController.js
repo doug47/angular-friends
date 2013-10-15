285b4abab17e827c403ffa5d1668547d6185f5d3
Colt Henrie <colt@Admins-MacBook-Pro.local>
2013-10-15 11:15:50 -0600

    first push

--- FriendController.js
+++ FriendController.js
@@ -1,3 +1,12 @@
+app.controller ('AppCtrl', function ($scope, $http) {
+
+	$http.get("https://s3.amazonaws.com/intuiplan_company_files/production/files/public/FriendData.json").then(function (data){
+		$scope.list = data.data;
+		//var friends = $scope.list
+		console.log(data);
+
+});
+});
 
 
 
